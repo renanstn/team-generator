4251fe3d7ee6dd15f6a805e29adfcbbd85834cef
Renan Santana <renan_ota@hotmail.com>
2023-05-15 10:04:04 -0300

    Add base tests

---
+++ team_generator/test.py
@@ -0,0 +1,33 @@
+import unittest
+
+
+class TestAPI:
+    def test_hello():
+        """
+        Test the ping endpoint.
+        """
+        assert False
+
+    def test_join_event():
+        """
+        It must be possible for a user to join an upcoming event.
+        """
+        assert False
+
+    def test_join_duplicated_user():
+        """
+        The same user can not join the same event twice.
+        """
+        assert False
+
+    def test_generate_teams():
+        """
+        It must be possible for an admin user to generate teams for an event.
+        """
+        assert False
+
+    def test_print_teamboard():
+        """
+        It must be possible to get a team board PDF file for print.
+        """
+        assert False
