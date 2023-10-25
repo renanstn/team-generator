4fb96ca44efbe7775fe0c979cd6fa8bac1670108
Renan Santana <renan.santana@instruct.com.br>
2023-10-25 15:29:30 -0300

    (backend): Add docstrings

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -10,11 +10,18 @@ class TestAPI(unittest.TestCase):
         self.client = TestClient(app)
 
     def test_hello(self):
+        """
+        The hello endpoint must return a simple message.
+        """
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {"Hello": "World"})
 
     def test_database_connection(self):
+        """
+        The test_db endpoint must connect to the database to get some data and
+        return the value loaded.
+        """
         response = self.client.get("/test_db")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {"Stored value": "test"})
