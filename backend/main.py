4122b019051e34f6ccec9805f4d96cc40f0fdc3d
Renan Santana <renan.santana@instruct.com.br>
2023-10-25 15:17:47 -0300

    (backend): Add docstrings

--- backend/main.py
+++ backend/main.py
@@ -11,10 +11,16 @@ app = FastAPI()
 
 @app.get("/")
 async def hello():
+    """
+    An endpoint for connection test purposes.
+    """
     return {"Hello": "World"}
 
 
 @app.get("/test_db")
 async def test_database_connection(db: Session = Depends(get_db)):
+    """
+    An endpoint for database connection test purposes.
+    """
     data = db.query(Hello).first()
     return {"Stored value": data.name} if data else {"The database is empty."}
